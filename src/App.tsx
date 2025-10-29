a395dd6ad83101b92b8e6bfa3b66aaf2f81d7f91
Jeevan kumar Chowdapu <jeevankumarchowdapu@Jeevans-MacBook-Pro.local>
2025-10-28 20:25:09 -0400

    Some initial fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, ReactNode } from "react";
+import { createContext, useState, useEffect, type ReactNode } from "react";
 
 interface AuthContextType {
   user: any;
